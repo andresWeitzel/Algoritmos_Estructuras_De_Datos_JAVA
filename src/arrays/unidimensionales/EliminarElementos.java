54d561c6f2c09e3d10017588a9310ad9b8c9a1ab
andres <andres06energy@hotmail.com>
2022-03-10 23:16:07 -0300

    Updated Proyect

--- src/arrays/unidimensionales/EliminarElementos.java
+++ src/arrays/unidimensionales/EliminarElementos.java
@@ -4,55 +4,90 @@ public class EliminarElementos {
 
 	public static void main(String[] args) {
 		
-		int array[]=new int[20];
-		int arrayTemporal[]=new int[20];
+		
+		int array[] = new int[20];
+		
+		int arrayTemporal[] = new int[array.length];
+
+		
+		
 		
 		
 		System.out.println("\n=== Array de Números ===");
-		for(int i=0; i< array.length ; i++) {
-			
-			array[i]= 2 + i;
-			
+		for (int i = 0; i < array.length; i++) {
+
+			array[i] = 1 + i;
+
 			System.out.println(array[i]);
 		}
+
+		
+		
 		
 		
 		
 		System.out.println("\n==== Eliminando Elementos ====");
-		
-		for(int i=0; i< array.length ; i++) {
-			
-			if(array[i] == 12 ) {
+
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i] <= array.length) {
+
+				if (array[i] == 12) {
+
+					array[i] = 0;
+					
+				    //System.out.println(array[i]);;
+					
+					arrayTemporal[i] = array[i+1] ;
+
+				}
+
+				else if ((!(array[i] == 0)) && (!(arrayTemporal[i] == arrayTemporal[0]))) {
+
+					if((arrayTemporal[i] == arrayTemporal[i - 1])) {
+						
+						
+						arrayTemporal[i] = array[i+1];
+
+						System.out.println(array[i]);
+
+					}else {
+						
+						arrayTemporal[i] = array[i] ;
+						
+						System.out.println(array[i]);
+						
+					}
+					
+				}else if(arrayTemporal[i] == arrayTemporal[0]){
 				
-				array[i] = 0;
+					arrayTemporal[i] = array[i] ;
+					
+					System.out.println(array[i]);
 				
+				}else {
 				
+
+					return;
+					//arrayTemporal[i] = arrayTemporal[i];
+					//arrayTemporal[i] = array[i+1] ;
+				}
+
 			}
-			
-			else if( !(array[i] == 0) ) {
-				
-			
-			
-				System.out.println(arrayTemporal[i]);
-				
-				
-			}else {
-				
-				array[i]=array[i];
-			}
-			
-		
+
 		}
 		
 		
+
 		System.out.println("\n=== Array Depurado ===");
-		
-		for(int i=0; i< arrayTemporal.length ; i++) {
-			
-			System.out.println(arrayTemporal[i]);	
-			
+
+		for (int i = 0; i < arrayTemporal.length; i++) {
+
+			System.out.println(arrayTemporal[i]);
+
 		}
-		
+
+
 	}
-	
+
 }
